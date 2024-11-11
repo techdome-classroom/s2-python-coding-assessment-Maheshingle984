526e7b915016c31380cd89c8826290e93fe87604
Arjunmachidi <146446642+Arjunmachidi@users.noreply.github.com>
2024-11-11 15:49:47 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -10,7 +10,30 @@ class Solution(object):
 
 
 
-
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        # Mapping of closing brackets to their respective opening brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map:
+                # Pop the topmost element if the stack is not empty, otherwise assign a dummy value
+                top_element = stack.pop() if stack else '#'
+                
+                # If the mapping for the closing bracket doesn't match the stack's top element, return False
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push onto the stack
+                stack.append(char)
+        
+        # If the stack is empty, all brackets are closed properly
+        return not stack
 
     
 
--- program2.md
+++ program2.md
@@ -2,7 +2,6 @@
 
 # Problem Statement:
 Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
 Symbol       Value
 I             1
 V             5
@@ -19,9 +18,6 @@ I can be placed before V (5) and X (10) to make 4 and 9.
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
-
- 
-
 Example 1:
 
 Input: s = "III"
@@ -37,10 +33,7 @@ Example 3:
 Input: s = "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- 
-
 Constraints:
-
 1 <= s.length <= 15
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
